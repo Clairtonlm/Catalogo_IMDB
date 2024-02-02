ce834eba766a0e20064406fd0b3deeca37e4d804
Clairtonlm <clairtonlm@gmail.com>
2024-02-02 20:11:16 -0300

    Class Filme

--- CatalagoFilmes/CatalagoFilmes/src/tech/clairtonlima/t1113/model/Filme.java
+++ CatalagoFilmes/CatalagoFilmes/src/tech/clairtonlima/t1113/model/Filme.java
@@ -1,12 +1,11 @@
 package tech.clairtonlima.t1113.model;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class Filme {
     private String nome;
     private LocalDate dataLancamento;
-    private BigDecimal orcamento;
+    private double orcamento;
     private  String descricao;
     private List<Ator> atores;
     private List<Diretor> diretores;
